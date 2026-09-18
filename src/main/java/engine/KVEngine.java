05698da1376dd8ec39d2c927e92c710a9e2153ba
Chiemela <onyensochiemela24@gmail.com>
2026-09-17 23:17:54 -0400

    feat: add ConcurrentHashMap store and KVEngine constructor

--- src/main/java/engine/KVEngine.java
+++ src/main/java/engine/KVEngine.java
@@ -1,4 +1,15 @@
 package engine;
 
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class KVEngine {
+
+    private final ConcurrentHashMap<String, String> store;
+
+    // Constructor
+    // Create an empty map in memory whenever a new KVEngine object is created
+    public KVEngine() {
+        this.store = new ConcurrentHashMap<>();
+    }
 }
