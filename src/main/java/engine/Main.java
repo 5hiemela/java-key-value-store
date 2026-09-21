bc34e66942afb8b487a161de741dea7bb8fdb0ef
Chiemela <onyensochiemela24@gmail.com>
2026-09-20 22:36:34 -0400

    test: add tests for put, get, delete, etc in Main

--- src/main/java/engine/Main.java
+++ src/main/java/engine/Main.java
@@ -3,5 +3,21 @@ package engine;
 public class Main {
     public static void main(String[] args) {
         KVEngine engine = new KVEngine();
+
+        // Test put and get + size
+        engine.put("player_1", "Guts");
+        engine.put("player_2", "Griffith");
+
+        System.out.println("PLayer 1: " + engine.get("player_1").orElse("Not Found"));
+        System.out.println("Total keys stored: " + engine.size());
+
+        // Test delete and containsKey
+        engine.delete("player_2");
+        System.out.println("Player 2 exists: " + engine.containsKey("player)2"));
+        System.out.println("Total keys after deletion: " + engine.size());
+
+        // Test clear
+        engine.clear();
+        System.out.println("Total keys after clear: " + engine.size());
     }
 }
